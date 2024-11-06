d4f8d37581769181a15a6c802054b09b773f104b
DeepaS09 <maildeepavijapurkar@gmail.com>
2024-11-06 10:43:08 +0800

    Screenshot 2024-11-06 at 10.30.14 AM

---
+++ Javatask1.java
@@ -0,0 +1,19 @@
+package javaTask;
+
+public class Javatask {
+
+	public static void main(String[] args) {
+		int a=100;
+		int b=200;
+		int c=300;
+		int d=400;
+		if ((a+b) > (c+d)){
+			System.out.println("sum of a and b is greater than sum of c and d");
+		} else {
+			System.out.println("Value of a and b is not greater than sum of c and d");
+		}
+		}
+
+	}
+
+
